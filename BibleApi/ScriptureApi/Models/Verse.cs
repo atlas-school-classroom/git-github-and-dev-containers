77d505302d486e0d00aefd9730165119954b0bb7
David D. Smith <smith74066@outlook.com>
2026-04-12 06:05:58 +0000

    deleted files

--- BibleApi/ScriptureApi/Models/Verse.cs
+++
@@ -1,7 +0,0 @@
-public class Verse
-{
-    public int Id { get; set; }
-    public string Reference { get; set; } = string.Empty;
-    public string Text { get; set; } = string.Empty;
-    public string Category { get; set;} = string.Empty;
-}
